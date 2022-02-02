4c4a7e6a2243316993f30ffb2715d52e8463db33
Tippi Kulkij <koaatang@yahoo.com>
2022-02-02 17:15:02 -0500

    delete test_willDelete

--- test_willDelete.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-library(caret)
-
-# set.seed(1) # if using R 3.5 or earlier
-set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
-n <- 100
-Sigma <- 9*matrix(c(1.0, 0.5, 0.5, 1.0), 2, 2)
-dat <- MASS::mvrnorm(n = 100, c(69, 69), Sigma) %>%
-  data.frame() %>% setNames(c("x", "y"))
