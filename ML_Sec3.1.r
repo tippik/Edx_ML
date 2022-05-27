e79b6e5d0fe3ff246349287555c5da7ffc9ea654
hadf <koaat@yahoo.com>
2022-05-27 12:44:59 -0400

    Commit ML Sec3.1

--- ML_Sec3.1.r
+++ ML_Sec3.1.r
@@ -1,9 +1,159 @@
 library(tidyverse)
 library(caret)
-set.seed(1, sample.kind = "Rounding")
+set.seed(1, sample.kind="Rounding") 
 n<-100
-Singma <- 9*martix(c(1.0, 0.5, 0.5,.1.0), 2, 2)
-dat ,-MASS::mvrnorm(n=100, c(69,69), Sigma)%>%
+Singma <- 9*matrix(c(1.0, 0.5, 0.5,1.0), 2, 2)
+dat <-MASS::mvrnorm(n=100, c(69,69), Singma)%>%
   data.frame()%>%setNames(c("x","y"))
-  delete this line
-  delet this line
+#1. create partition into est and training set p=0.5 using dat$y#
+test_index <- createDataPartition(dat$y, times = 1, p=0.5, list = FALSE)
+#2. train a linear model predicting y from x#
+train_set <- dat %>% slice(-test_index)
+test_set <- dat %>% slice(test_index)
+fit <- lm(y~x, data=train_set)
+fit$coefficients
+#3. generate predictions on the test set#
+pre_dict <- fit$coef[1] + fit$coef[2]*test_set$x
+#or#
+pre_dict2 <- predict(lm(y~x, data=train_set), test_set)
+if (pre_dict==pre_dict2){print("equal")}
+#4. calculate RMSE of the pre_dict RMSE= sqrt(mean(data$actual - data$predicted)^2)#
+#M69.278, SD7.786, wrong#
+RMSE_100 <- replicate(100, {
+  test_index <- createDataPartition(dat$y, times = 1, p=0.5, list = FALSE)
+  train_set <- dat %>% slice(-test_index)
+  test_set <- dat %>% slice(test_index)
+  pre_dict2 <- predict(lm(y~x, train_set), test_set)
+  sqrt(mean((pre_dict2-test_set$y)^2))
+  })
+mean(RMSE_100)
+sd(RMSE_100)
+# solution, Q1#
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+rmse <- replicate(100, {
+  test_index <- createDataPartition(dat$y, times = 1, p = 0.5, list = FALSE)
+  train_set <- dat %>% slice(-test_index)
+  test_set <- dat %>% slice(test_index)
+  fit <- lm(y ~ x, data = train_set)
+  y_hat <- predict(fit, newdata = test_set)
+  sqrt(mean((y_hat-test_set$y)^2))
+})
+
+mean(rmse)
+sd(rmse)
+
+
+#Q2#
+number <- c(100, 500, 1000, 5000, 10000)
+
+rmse_fun <- function(number) {
+  Singma <- 9*matrix(c(1.0, 0.5, 0.5,1.0), 2, 2)
+  dat <-MASS::mvrnorm(n=number, c(69,69), Singma)%>%
+    data.frame()%>%setNames(c("x","y"))
+  fun <- replicate(100, {
+  test_index <- createDataPartition(dat$y, times = 1, p = 0.5, list = FALSE)
+  train_set <- dat %>% slice(-test_index)
+  test_set <- dat %>% slice(test_index)
+  fit <- lm(y ~ x, data = train_set)
+  y_hat <- predict(fit, newdata = test_set)
+  sqrt(mean((y_hat-test_set$y)^2))
+})}
+set.seed(1, sample.kind="Rounding") 
+Q2 <- sapply(number, rmse_fun)
+mean(Q2[,1])
+sd(Q2[,1])
+mean(Q2[,2])
+sd(Q2[,2])
+mean(Q2[,3])
+sd(Q2[,3])
+mean(Q2[,4])
+sd(Q2[,4])
+mean(Q2[,5])
+sd(Q2[,5])
+#Q4
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+n <- 100
+Sigma <- 9*matrix(c(1.0, 0.95, 0.95, 1.0), 2, 2)
+dat <- MASS::mvrnorm(n = 100, c(69, 69), Sigma) %>%
+  data.frame() %>% setNames(c("x", "y"))
+set.seed(1, sample.kind="Rounding")
+rmse <- replicate(100, {
+  test_index <- createDataPartition(dat$y, times = 1, p = 0.5, list = FALSE)
+  train_set <- dat %>% slice(-test_index)
+  test_set <- dat %>% slice(test_index)
+  fit <- lm(y ~ x, data = train_set)
+  y_hat <- predict(fit, newdata = test_set)
+  sqrt(mean((y_hat-test_set$y)^2))
+})
+mean(rmse)
+sd(rmse)
+#Q6
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+Sigma <- matrix(c(1.0, 0.75, 0.75, 0.75, 1.0, 0.25, 0.75, 0.25, 1.0), 3, 3)
+dat <- MASS::mvrnorm(n = 100, c(0, 0, 0), Sigma) %>%
+  data.frame() %>% setNames(c("y", "x_1", "x_2"))
+cor(dat)
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+test_index <- createDataPartition(dat$y, times = 1, p = 0.5, list = FALSE)
+train_set <- dat %>% slice(-test_index)
+test_set <- dat %>% slice(test_index)
+
+fit <- lm(y ~ x_1, data = train_set)
+y_hat <- predict(fit, newdata = test_set)
+sqrt(mean((y_hat-test_set$y)^2))
+
+fit <- lm(y ~ x_2, data = train_set)
+y_hat <- predict(fit, newdata = test_set)
+sqrt(mean((y_hat-test_set$y)^2))
+
+fit <- lm(y ~ x_1 + x_2, data = train_set)
+y_hat <- predict(fit, newdata = test_set)
+sqrt(mean((y_hat-test_set$y)^2))
+#Q8
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+Sigma <- matrix(c(1.0, 0.75, 0.75, 0.75, 1.0, 0.95, 0.75, 0.95, 1.0), 3, 3)
+dat <- MASS::mvrnorm(n = 100, c(0, 0, 0), Sigma) %>%
+  data.frame() %>% setNames(c("y", "x_1", "x_2"))
+cor(dat)
+set.seed(1, sample.kind="Rounding") # if using R 3.6 or later
+test_index <- createDataPartition(dat$y, times = 1, p = 0.5, list = FALSE)
+train_set <- dat %>% slice(-test_index)
+test_set <- dat %>% slice(test_index)
+
+fit <- lm(y ~ x_1, data = train_set)
+y_hat <- predict(fit, newdata = test_set)
+sqrt(mean((y_hat-test_set$y)^2))
+
+fit <- lm(y ~ x_2, data = train_set)
+y_hat <- predict(fit, newdata = test_set)
+sqrt(mean((y_hat-test_set$y)^2))
+
+fit <- lm(y ~ x_1 + x_2, data = train_set)
+y_hat <- predict(fit, newdata = test_set)
+sqrt(mean((y_hat-test_set$y)^2))
+#comprehension Check Q1
+set.seed(2, sample.kind="Rounding") #if you are using R 3.6 or later
+make_data <- function(n = 1000, p = 0.5, 
+                      mu_0 = 0, mu_1 = 2, 
+                      sigma_0 = 1,  sigma_1 = 1){
+  
+  y <- rbinom(n, 1, p)
+  f_0 <- rnorm(n, mu_0, sigma_0)
+  f_1 <- rnorm(n, mu_1, sigma_1)
+  x <- ifelse(y == 1, f_1, f_0)
+  
+  test_index <- createDataPartition(y, times = 1, p = 0.5, list = FALSE)
+  
+  list(train = data.frame(x = x, y = as.factor(y)) %>% slice(-test_index),
+       test = data.frame(x = x, y = as.factor(y)) %>% slice(test_index))
+}
+dat <- make_data()
+set.seed(1, sample.kind="Rounding") #if you are using R 3.6 or later
+delta <- seq(0, 3, len = 25)
+res <- sapply(delta, function(d){
+  dat <- make_data(mu_1 = d)
+  fit_glm <- dat$train %>% glm(y ~ x, family = "binomial", data = .)
+  y_hat_glm <- ifelse(predict(fit_glm, dat$test) > 0.5, 1, 0) %>% factor(levels = c(0, 1))
+  mean(y_hat_glm == dat$test$y)
+})
+qplot(delta, res)
---
+++ ML_Sec3.2.r
@@ -0,0 +1,54 @@
+library(lubridate)
+library(purrr)
+library(pdftools)
+
+fn <- system.file("extdata", "RD-Mortality-Report_2015-18-180531.pdf", package="dslabs")
+dat <- map_df(str_split(pdf_text(fn), "\n"), function(s){
+  s <- str_trim(s)
+  header_index <- str_which(s, "2015")[1]
+  tmp <- str_split(s[header_index], "\\s+", simplify = TRUE)
+  month <- tmp[1]
+  header <- tmp[-1]
+  tail_index  <- str_which(s, "Total")
+  n <- str_count(s, "\\d+")
+  out <- c(1:header_index, which(n==1), which(n>=28), tail_index:length(s))
+  s[-out] %>%
+    str_remove_all("[^\\d\\s]") %>%
+    str_trim() %>%
+    str_split_fixed("\\s+", n = 6) %>%
+    .[,1:5] %>%
+    as_data_frame() %>% 
+    setNames(c("day", header)) %>%
+    mutate(month = month,
+           day = as.numeric(day)) %>%
+    gather(year, deaths, -c(day, month)) %>%
+    mutate(deaths = as.numeric(deaths))
+}) %>%
+  mutate(month = recode(month, "JAN" = 1, "FEB" = 2, "MAR" = 3, "APR" = 4, "MAY" = 5, "JUN" = 6, 
+                        "JUL" = 7, "AGO" = 8, "SEP" = 9, "OCT" = 10, "NOV" = 11, "DEC" = 12)) %>%
+  mutate(date = make_date(year, month, day)) %>%
+  dplyr::filter(date <= "2018-05-01")
+total_days <- diff(range(dat$date))
+total_days <- as.numeric(1216)
+span <- 60/as.numeric(total_days)
+fit <- loess(deaths ~ as.numeric(date), degree = 1, span = span, data=dat)
+dat %>% ggplot(aes(date,deaths))+geom_point()+geom_smooth()
+dat %>% ggplot(aes(date,deaths))+geom_point()+geom_smooth(span = span, method = "loess", method.args = list(degree=1))
+#solutions for Q1
+span <- 60 / as.numeric(diff(range(dat$date)))
+fit <- dat %>% mutate(x = as.numeric(date)) %>% loess(deaths ~ x, data = ., span = span, degree = 1)
+dat %>% mutate(smooth = predict(fit, as.numeric(date))) %>%
+  ggplot() +
+  geom_point(aes(date, deaths)) +
+  geom_line(aes(date, smooth), lwd = 2, col = "red")
+
+
+#Q3
+library(broom)
+library(dslabs)
+mnist_27$train %>% glm(y ~ x_2, family = "binomial", data = .) %>% tidy()
+qplot(x_2, y, data = mnist_27$train)
+mnist_27$train %>% 
+  mutate(y = ifelse(y=="7", 1, 0)) %>%
+  ggplot(aes(x_2, y)) + 
+  geom_smooth(method = "loess")
